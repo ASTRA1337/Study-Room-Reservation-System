40582cd61e247ba423350b55394763060cbfb8b7
Kody Smith <kwsmith90@gmail.com>
2021-11-09 16:23:59 -0600

    Made file adjustments and Did Topbar Component

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./welcome.css";
+import LoginForm from '../Forms/LoginForm';
+
+
+
+
+
+function Welcome() {
+    
+    return (
+        <div className = "welcomepage">
+           welcome !!!
+        </div>
+        
+    )
+}
+
+export default Welcome
