e7ba4982be13d15d5238232ec6355325abe91e3f
Minh Nguyen <tmnguyen1403@gmail.com>
2021-12-02 01:22:48 -0600

    Format time to 24hour time

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -3,30 +3,37 @@ import {useMutation} from 'react-query';
 import {makeReservation} from '../API';
 import './Reservation.css';
 
-function Reservation({}) {
+function convertTo24HourFormat(time) {
+    var addHour = 0;
+    if (time.includes("PM"))
+        addHour = 12;
+    var t = time.replace(/[AM|PM|am|pm|\s]/g,""); //remove all space, AM, PM from time
+    var tmp = t.split(":");
+    var hour = parseInt(tmp[0]) % 12 + addHour;
+
+    return hour + ":" + tmp[1];
+}
+
+function Reservation({reservations}) {
     const mutation = useMutation(makeReservation, {
         onSuccess: (data) => {
             console.log("Reservation success", data);
         }
     });
+    const {user_id, room_id, date, times} = reservations;
+    //times should be an array of object with format {start, end}
+    const conformTimes = times.map((t) => {
+        var start = convertTo24HourFormat(t.start);
+        var end = convertTo24HourFormat(t.end);
+        return {start: date + " " + start,
+                end: date + " " + end}
+    });
     const data = {
         user_id: 1,
         room_id: 2,
-        time_blocks:[{
-            start: "2021/12/02 10:00",
-            end: "2021/12/02 10:30"
-        },
-        {
-            start: "2021/12/02 10:30",
-            end: "2021/12/02 11:00"
-        },
-        {
-            start: "2021/12/02 11:00",
-            end: "2021/12/02 11:30"
-        }
-        ],
-
+        time_blocks:conformTimes,
     };
+
     return (
         <button onClick={() => mutation.mutate(data)}> Reservation</button>
     )
